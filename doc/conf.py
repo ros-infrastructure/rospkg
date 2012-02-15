de98cb602e20e8dcff88438283322843ffb6e908
Ken Conley <kwc@willowgarage.com>
2012-02-15 15:06:39 -0800

    ros theme

--- doc/conf.py
+++ doc/conf.py
@@ -99,12 +99,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+html_theme = 'ros-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+
+html_logo = 'ros.png'
+html_theme_path = [os.path.join(os.path.expanduser('~'), 'sphinx'), 'themes']
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
