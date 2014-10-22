7d8aa9e9cc2d4cd3dbe7d8907bdabfd5a485869f
Dirk Thomas <dthomas@osrfoundation.org>
2014-10-22 15:41:21 -0700

    only depend on argparse for Python < 2.7

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@
 
 import os
 from setuptools import setup
+import sys
 
 exec(open(os.path.join(os.path.dirname(__file__), 'src', 'rospkg', '_version.py')).read())
+
+install_requires = ['PyYAML']
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    install_requires.append('argparse')
 
 setup(name='rospkg',
       version=__version__,
       packages=['rospkg'],
       package_dir = {'':'src'},
       scripts = ['scripts/rosversion'],
-      install_requires=['argparse', 'PyYAML'],
+      install_requires=install_requires,
       author = "Ken Conley", 
       author_email = "kwc@willowgarage.com",
       url = "http://wiki.ros.org/rospkg",
