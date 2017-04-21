eb4af2a59eae96bbe1db73dd2e968476c867ef5b
Tidrek <freethinking@inbox.ru>
2017-04-21 11:14:56 +0500

    Fix os_detect to correctly detect Elementary OS (#116)
    * Fix os_detect to correctly detect Elementary OS
    
    * Detect Elementary OS properly on the newest and older releases

--- src/rospkg/os_detect.py
+++ src/rospkg/os_detect.py
@@ -669,6 +669,7 @@ OS_CENTOS = 'centos'
 OS_CYGWIN = 'cygwin'
 OS_DEBIAN = 'debian'
 OS_ELEMENTARY = 'elementary'
+OS_ELEMENTARY_OLD = 'elementary'
 OS_FEDORA = 'fedora'
 OS_FREEBSD = 'freebsd'
 OS_FUNTOO = 'funtoo'
@@ -690,7 +691,8 @@ OsDetect.register_default(OS_MANJARO, Manjaro())
 OsDetect.register_default(OS_CENTOS, Centos())
 OsDetect.register_default(OS_CYGWIN, Cygwin())
 OsDetect.register_default(OS_DEBIAN, Debian())
-OsDetect.register_default(OS_ELEMENTARY, LsbDetect('elementary OS'))
+OsDetect.register_default(OS_ELEMENTARY, LsbDetect('"elementary"'))
+OsDetect.register_default(OS_ELEMENTARY_OLD, LsbDetect('"elementary OS"'))
 OsDetect.register_default(OS_FEDORA, FdoDetect("fedora"))
 OsDetect.register_default(OS_FREEBSD, FreeBSD())
 OsDetect.register_default(OS_FUNTOO, Funtoo())
