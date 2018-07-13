5a03a29559796f36eea3ca578a61934a8dcd9d72
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2018-07-13 10:57:31 -0700

    [get_licenses] Accept multiple packages' name to begin the license introspection with.

--- src/rospkg/sw_license.py
+++ src/rospkg/sw_license.py
@@ -31,13 +31,15 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from collections import OrderedDict
+from collections import defaultdict, OrderedDict
 import logging
-from rospkg import ResourceNotFound, RosPack
 import yaml
+
+from rospkg import ResourceNotFound, RosPack
 
 
 class LicenseUtil(object):
+    _URL_ISSUE_RELEVANT = "https://github.com/ros-infrastructure/rospkg/pull/129#issue-168007083"
 
     def __init__(self):
         self.rp = RosPack()
@@ -69,26 +71,77 @@ class LicenseUtil(object):
                 logging.info("License '{}' was present.".format(key))
         return ret
 
-    def software_license(self, pkgname):
+    def software_license(self, pkgnames):
         """
-        @return List of licenses and packages.
+        @type pkgnames: Either one of the following:
+                    - [str]: list of str.
+                    - str str...str: space separated string.
+        @param pkgnames: Name of one or more packages to start software license
+                       introspection from (i.e. leaf packages in the dependency chain).
+        @return List of software licenses found in the dependency chain started from
+                       the package names passed in 'pkgnames'. When multiple packages
+                       passed to start with, union of all the results is returned.
         @raise AttributeError, ResourceNotFound
         """
-        if not pkgname:
-            raise ValueError("Argument was insufficient: pkgname {}".format(pkgname))
-        try:
-            dict_license = self.rp.get_licenses(pkgname)
-        except AttributeError as e:
-            raise e
-        except ResourceNotFound as e:
-            URL_ISSUE_RELEVANT="https://github.com/ros-infrastructure/rospkg/pull/129#issue-168007083"
-            logging.warn("{}\nRe-run get_licenses to work around an issue with get_depends (see {} if needed).".format(repr(e), URL_ISSUE_RELEVANT))
-            dict_license = self.rp.get_licenses(pkgname)
-        logging.debug(dict_license)
-        return dict_license
+        if not pkgnames:
+            raise ValueError("Argument was insufficient: pkgname {}".format(pkgnames))
+
+        # Check if pkgnames is str, not list.
+        if isinstance(pkgnames, basestring):
+            # If space(s) are found, 1) take it as a single package name if only one str
+            # element is found. 2) Convert to [str] if multiple str elements found.
+            pkgnames = pkgnames.split()
+
+        # Could store multiple resulted dicts for a package
+        dicts_of_result = []
+        for pkg_name in pkgnames:
+            try:
+                dict_license = self.rp.get_licenses(pkg_name)
+            except AttributeError as e:
+                raise e
+            except ResourceNotFound as e:
+                logging.warn(
+                    "{}\nRe-running Rospack.get_licenses to work around an issue with"
+                    " get_depends (see {} if needed).".format(
+                        repr(e), self._URL_ISSUE_RELEVANT))
+                dict_license = self.rp.get_licenses(pkg_name)
+            dicts_of_result.append(dict_license)
+
+        # Take the union of the results.
+        dict_licenses = self._union_dicts(dicts_of_result)
+        logging.debug(dict_licenses)
+        return dict_licenses
+
+    def _union_dicts(self, d):
+        """
+        @param *d: dictionaries, each of which needs to be formatted as the output of
+                       rospkg.RosPack.get_licenses (otherwise), i.e. { k, [d] }.
+        @rtype { k, [d] }
+        """
+        # https://stackoverflow.com/a/14766978/577001
+
+        logging.debug("d: {} len(d): {}\n".format(d, len(d)))
+
+        newdicts = defaultdict(set)  # Define a defaultdict
+        for each_dict in d:
+            logging.debug("each_dict: {}\n".format(each_dict))
+            #ordered_dic = OrderedDict(sorted(list(each_dict)[0].items()))
+            ordered_dic = OrderedDict(sorted(each_dict.items()))
+
+            # dict.items() returns a list of (k, v) tuple.
+            # So, you can directly unpack the tuple in two loop variables.
+            for k, v in ordered_dic.items():
+                newdicts[k] |= set(v)
+
+            logging.debug("newdicts: {}\n".format(newdicts))
+        # And if you want the exact representation that you have shown
+        # You can build a normal dict out of your newly built dict.
+        union = {key: sorted(list(value)) for key, value in newdicts.items()}
+        logging.debug(union)
+        return union
 
     def save_licenses(
-            self, licenses, pkgname, implicit=True, sortbylicense=True,
+            self, licenses, pkgnames, implicit=True, sortbylicense=True,
             prefix_outfile="/tmp/licenses", description_output=None):
         """
         @summary:  If True save the result of get_licenses to a text file.
@@ -104,14 +157,25 @@ class LicenseUtil(object):
         @raise ResourceNotFound
         """
         pkg_version = "pkgversion"
-        if not description_output:
-            description_output = "# Output of software license introspection started from {}.".format(pkgname)
 
-        try:
-            pkg_version = self.rp.get_manifest(pkgname).version
-        except Exception as e:
-            print(str(e))
-        path_outputfile = '{}_{}-{}.yml'.format(prefix_outfile, pkgname, pkg_version)
+        if isinstance(pkgnames, basestring):
+            pkgnames = pkgnames.split()
+
+        if not description_output:
+            description_output = """# Output of software license introspection started from {}""".format(pkgnames)
+
+        pkgnames_versions = []
+        for pkgname in pkgnames:
+            try:
+                pkg_version = self.rp.get_manifest(pkgname).version
+                pkgnames_versions.append("{}-{}".format(pkgname, pkg_version))
+            except Exception as e:
+                print(str(e))
+
+        # Delimits package name with underscore.
+        pkgnames_versions_str = "_".join(pkgnames_versions)
+
+        path_outputfile = '{}-{}.yml'.format(prefix_outfile, pkgnames_versions_str)
         with open(path_outputfile, 'w') as outfile:
             outfile.write("{}\n".format(description_output))
             yaml.dump(licenses, outfile, default_flow_style=False, allow_unicode=True)
