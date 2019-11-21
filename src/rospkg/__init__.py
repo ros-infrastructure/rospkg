313a880e2e1c5e122b31ba9aaf2844b1dbf187a9
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-11-21 15:12:25 -0800

    1.2.0

--- src/rospkg/__init__.py
+++ src/rospkg/__init__.py
@@ -45,7 +45,7 @@ from .rospack import expand_to_packages, get_package_name, \
 # same version as in:
 # - setup.py
 # - stdeb.cfg
-__version__ = '1.1.10'
+__version__ = '1.2.0'
 
 __all__ = (
     'MANIFEST_FILE', 'ResourceNotFound', 'STACK_FILE',
