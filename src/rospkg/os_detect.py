017549e597581d0f14bc12d11f60e8ca6747752b
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-12-12 14:44:18 -0800

    force absolute imports

--- src/rospkg/os_detect.py
+++ src/rospkg/os_detect.py
@@ -32,6 +32,7 @@ Library for detecting the current OS, including detecting specific
 Linux distributions.
 """
 
+from __future__ import absolute_import
 from __future__ import print_function
 
 import codecs
