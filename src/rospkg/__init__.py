1d27a214d33a568e2392e8de7825372f5d55bef6
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-04-03 14:47:01 -0400

    1.2.4

--- src/rospkg/__init__.py
+++ src/rospkg/__init__.py
@@ -45,7 +45,7 @@ from .rospack import expand_to_packages, get_package_name, \
 # same version as in:
 # - setup.py
 # - stdeb.cfg
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 __all__ = (
     'MANIFEST_FILE', 'ResourceNotFound', 'STACK_FILE',
