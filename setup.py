d36a67b33ae3dfe80a9f41d458dab29ae2317fbb
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-04-20 14:07:53 -0700

    can't list distro as a dependency in the Debian packages since the same package is used across multiple Python versions

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ if sys.version_info[0] == 2 and sys.version_info[1] < 7:
 if (
     sys.version_info[0] < 3 or
     (sys.version_info[0] == 3 and sys.version_info[1] < 8) or
-    'SKIP_PYTHON_MODULES' in os.environ
+    'SKIP_PYTHON_MODULES' in os.environ or
+    'SKIP_PYTHON_SCRIPTS' in os.environ
 ):
     kwargs['install_requires'].remove('distro')
 
